d526dda787da17f40a82c9ede99b349a2532179b
Jinook Jung <jinookjung@Mac.lan>
2025-06-04 13:52:45 -0400

    update gitignore and page title

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ $cards = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello World</title>
+    <title>Load More Listings</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
